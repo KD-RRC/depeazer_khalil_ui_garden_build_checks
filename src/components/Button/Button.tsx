c127d8b8ccabdfd1e41122de77f1ca7a7ee0df00
kdepeazer <kdepeazer@rrc.ca>
2026-07-22 15:31:45 -0500

    test

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -35,7 +35,7 @@ const StyledButton = styled.button<{
 
 const Button: React.FC<ButtonProps> = ({
   label,
-  backgroundColor = '#3366ff',
+  backgroundColor = '#000000',
   textColor = '#ffffff',
   disabled = false,
   onClick,
